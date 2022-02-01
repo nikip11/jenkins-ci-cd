beb27a3ac1f024a927b18643fccb95ed0ea2994b
njponce <niki_ponce@hotmail.com>
2022-02-01 17:41:36 +0100

    commit update

--- index.php
+++ index.php
@@ -5,9 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello world</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,700&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Roboto', sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>Hello world</h1>
-    <?php echo 'Hello from PHP -- '; ?>
+    <h1>Hello world!</h1>
+    <?php echo 'Hello world! from PHP -- '; ?>
 </body>
 </html>
